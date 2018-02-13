95089a07c8822414906ab7f08a01cf568b621c20
Powerlated <zjia08@gmail.com>
2018-02-13 18:59:30 -0500

    Cleaned up a single line of code

--- folderbench.go
+++ folderbench.go
@@ -8,13 +8,14 @@ import (
 )
 
 func main() {
-	var version string = "v1.0"
+	version := "v1.0"
 
 	fmt.Println("You have started up folderbench " + version + ".")
 	fmt.Println()
 	askAgain := true
 	for askAgain {
-		foldercount := ask("How many folders?: ")
+		//foldercount := ask("How many folders?: ")
+
 	}
 
 }
