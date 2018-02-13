a183192fa30fc5644e30bebab15301031abf9516
Powerlated <zjia08@gmail.com>
2018-02-13 18:58:22 -0500

    very nonfunctional folder count asking

--- folderbench.go
+++ folderbench.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	input "github.com/tcnksm/go-input"
+)
 
 func main() {
 	var version string = "v1.0"
 
 	fmt.Println("You have started up folderbench " + version + ".")
 	fmt.Println()
+	askAgain := true
+	for askAgain {
+		foldercount := ask("How many folders?: ")
+	}
+
+}
+
+func ask(message string) string {
+	ui := &input.UI{
+		Writer: os.Stdout,
+		Reader: os.Stdin,
+	}
+
+	query := message
+	answer, err := ui.Ask(query, &input.Options{
+		Default:  "",
+		Required: true,
+		Loop:     false,
+	})
+	_ = err
+	return answer
 }
