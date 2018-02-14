f52b640c50da21ef746b36af1a4e478f343fe9e8
Powerlated <zjia08@gmail.com>
2018-02-13 19:23:44 -0500

    parsing system is fully working

--- folderbench.go
+++ folderbench.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	input "github.com/tcnksm/go-input"
 )
+
+var foldercount = 0
 
 func main() {
 	version := "v1.0"
 
 	fmt.Println("You have started up folderbench " + version + ".")
 	fmt.Println()
+
 	askAgain := true
 	for askAgain {
-		//foldercount := ask("How many folders?: ")
+		foldercount_str := ask("How many folders? (type exit to exit): ")
+		if _, err := strconv.ParseInt(foldercount_str, 10, 64); err != nil {
+			fmt.Println("It seems like that is not a number.")
+			fmt.Println()
+		} else {
+			askAgain = false
+			fmt.Println()
+			foldercount, err = strconv.Atoi(foldercount_str)
+			_ = err
 
+		}
 	}
 
 }
