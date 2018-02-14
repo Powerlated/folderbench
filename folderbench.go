8c9eba909a16ab2f7505a30a5db8007768ba4888
Powerlated <zjia08@gmail.com>
2018-02-13 21:45:31 -0500

    Revert "somethings have happened"
    This reverts commit e14c050f7669d9f7fbb927c98cf2b41718822128.
    
    Revert the mess that was caused by VSCODE

--- folderbench.go
+++ folderbench.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
+var defaultFoldercount = 250000
+
 func main() {
-	version := "v1.0"
+	version := "vl.0"
 
 	fmt.Println("You have started up folderbench " + version + ".")
 	fmt.Println()
+
+	foldercount := askFolderCount()
+	bench(foldercount)
+}
+func askFolderCount() int {
+	foldercount := 0
 	askAgain := true
 	for askAgain {
-		foldercount := ask("How many folders?: ")
-		
-	}
+		foldercount_str := ask("How many folders? (type exit to exit): ")
+		if foldercount_str == "exit" {
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		} else if foldercount_str == "" {
+			fmt.Println("It seems like no number has been provided. Using the default of " + strconv.Itoa(defaultFoldercount) + ".")
+			return defaultFoldercount
+		} else if _, err := strconv.ParseInt(foldercount_str, 10, 64); err != nil {
+			fmt.Println("It seems like that is not a number.")
+			fmt.Println()
+		} else {
+			askAgain = false
+			fmt.Println()
+			foldercount, err = strconv.Atoi(foldercount_str)
+			_ = err
 
+		}
+	}
+	return foldercount
 }
